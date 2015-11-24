7cdcf326565c7371f0d93b7fd567af830de63dd4
Nick Tomlin <nick.tomlin@gmail.com>
2015-11-23 19:18:28 -0600

    Add appveyor config and tweak protractor setup

--- example_spec.js
+++ example_spec.js
@@ -1,15 +1,37 @@
 describe('angularjs homepage', function() {
   it('should greet the named user', function() {
-    browser.get('http://ui-grid.info/docs/#/tutorial/190_large_dataset');
+    browser.get('http://www.angularjs.org');
 
-    var rows = element.all(by.repeater('(rowRenderIndex, row) in rowContainer.renderedRows track by $index'));
+    element(by.model('yourName')).sendKeys('Julie');
 
-    rows.count().then(console.log);
+    var greeting = element(by.binding('yourName'));
 
-    rows.each(function(element, index) {
-      element.getText().then(function (text) {
-        console.log(index, text);
-      });
+    expect(greeting.getText()).toEqual('Hello Julie!');
+  });
+
+  describe('todo list', function() {
+    var todoList;
+
+    beforeEach(function() {
+      browser.get('http://www.angularjs.org');
+
+      todoList = element.all(by.repeater('todo in todoList.todos'));
+    });
+
+    it('should list todos', function() {
+      expect(todoList.count()).toEqual(2);
+      expect(todoList.get(1).getText()).toEqual('build an angular app');
+    });
+
+    it('should add a todo', function() {
+      var addTodo = element(by.model('todoList.todoText'));
+      var addButton = element(by.css('[value="add"]'));
+
+      addTodo.sendKeys('write a protractor test');
+      addButton.click();
+
+      expect(todoList.count()).toEqual(3);
+      expect(todoList.get(2).getText()).toEqual('write a protractor test');
     });
   });
 });
