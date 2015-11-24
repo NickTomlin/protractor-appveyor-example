29f1b0cb0de48e5fb283ed440f76aef32d77d5f7
Nick Tomlin <nick.tomlin@gmail.com>
2015-11-23 22:16:27 -0600

    Simplify examples

--- example_spec.js
+++ example_spec.js
@@ -22,16 +22,5 @@ describe('angularjs homepage', function() {
       expect(todoList.count()).toEqual(2);
       expect(todoList.get(1).getText()).toEqual('build an angular app');
     });
-
-    it('should add a todo', function() {
-      var addTodo = element(by.model('todoList.todoText'));
-      var addButton = element(by.css('[value="add"]'));
-
-      addTodo.sendKeys('write a protractor test');
-      addButton.click();
-
-      expect(todoList.count()).toEqual(3);
-      expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
   });
 });
