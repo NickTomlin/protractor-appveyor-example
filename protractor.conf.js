53429eefd03a94ede4b0465c052c1ecf8699ba84
Nick Tomlin <nick.tomlin@gmail.com>
2015-11-25 14:01:18 -0600

    Do not use direct connect

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,20 +1,9 @@
-// An example configuration file.
 exports.config = {
-  directConnect: true,
-
-  // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'internet explorer'
   },
-
-  // Framework to use. Jasmine 2 is recommended.
   framework: 'jasmine2',
-
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
   specs: ['example_spec.js'],
-
-  // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   }
